e19f28d3dcb563efb495271de4488e4bc8daaabe
Sr.Marinho <79931083+heermarinho@users.noreply.github.com>
2021-09-24 02:59:28 +0000

    Initial commit

---
+++ definitions.py
@@ -0,0 +1,7 @@
+
+user = '57145a90-1cdb-11ec-ad90-75ec5e25c7a4'
+password = '259b73e5b99dfea5e05cb3e2e655f5cc5d16d9f5'
+client_id = '043863b0-1cdc-11ec-9f5b-45181495093e'
+server = "mqtt.mydevices.com"
+
+port = 1883
